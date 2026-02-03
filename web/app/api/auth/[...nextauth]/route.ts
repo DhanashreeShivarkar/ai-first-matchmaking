9cd0e853e030982751b604666189b5e56fca25dd
Dhanashree Shivarkar <dhanashreeshivarkar757@gmail.com>
2026-02-03 22:59:31 +0530

    User Login Done

--- web/app/api/auth/[...nextauth]/route.ts
+++ web/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,14 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt"
 
 const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -11,15 +18,41 @@ const handler = NextAuth({
       },
       async authorize(credentials) {
         // temporary dummy user
-        if (!credentials?.email) return null
+        if (!credentials?.email || !credentials?.password) {
+          return null
+        }
+        // 1 Find user in DB
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        })
+
+        if (!user || !user.password) {
+          return null
+        }
+
+        // 2 Compare password
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        )
+
+        if (!isValid) {
+          return null
+        }
 
         return {
-          id: "1",
-          email: credentials.email,
+          id: user.id,
+          name: user.name,
+          email: user.email,
         }
       },
     }),
   ],
+  pages: {
+    signIn: "/", // login page
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
 })
 
 export { handler as GET, handler as POST }
